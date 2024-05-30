07c691db020e10a7d72f47194b7358ebc22a6f8c
Derek Poitras <derek@logicsystem.ca>
2024-05-30 13:46:11 -0700

    established the second layer of the calc

--- ExposedOpeningCalc/residential.py
+++ ExposedOpeningCalc/residential.py
@@ -1,37 +1,39 @@
 import argparse
+import logging
 import sys
 import numpy as np
+import data
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
 
 class ResidentialCalc():
-    """Class that will handle calcualting for Residential, business and personal services,
+    """Class that will handle calculating for Residential, business and personal services,
     and low-hazard industrial occupation classification of buildings."""
 
-    def __init__(self):
-        self.area_list = [0, 30, 40, 50, 100, 101]
-        self.p30_x = np.array([
-            0.0,
-            1.2,
-            1.5,
-            2.0,
-            4.0,
-            6.0,
-            8.0,
-        ])
-        self.p30_y = np.array([
-            0,
-            7,
-            9,
-            12,
-            39,
-            88,
-            100,
-        ])
-
     def calc_values(self, exposed_face):
-        pass
+        face = data.z_area_list[0]
+        for v in data.z_area_list:
+            if v < exposed_face:
+                face = v
+        logger.debug(f'initial value {face} is type {type(face)}')
+        idx = data.z_area_list.index(face)
+        logger.debug(f'{exposed_face} uses index {idx}')
+        result = getattr(data, data.y_selection[idx])
+        logger.debug(f'return data {result}')
+        return result
+
     def calc_point(self, limit_dist, values):
-        x = self.p30_x
-        y = self.p30_y
+        if float(limit_dist) < float(data.x_limit_list[1]):
+            logger.debug(f'Small side {limit_dist} < {float(data.x_limit_list[1])}')
+            return 0.00
+        if float(limit_dist) > float(max(data.x_limit_list)):
+            logger.debug(f'Large side {limit_dist} > {float(max(data.x_limit_list))}')
+            return 100.00
+        logger.debug(f'passed {limit_dist}')
+        x = data.x_limit_list
+        y = data.y_30
         return np.interp(limit_dist, x, y)
 
 
@@ -43,6 +45,7 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--max_area', default=30)
     args = parser.parse_args()
     app = ResidentialCalc()
+    area = app.calc_values(105)
     res = app.calc_point(args.dist_limit, args.max_area)
-    print(res)
+    logger.debug(res)
     print(f'Maximum area is {res:.2f} square meters')
